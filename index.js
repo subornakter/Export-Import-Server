402693fecd7fc18d8279d1d6232ab689e945c543
subornakter <subornakter01@gmail.com>
2025-11-12 22:26:08 +0600

    add

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.get('/search', async (req, res) => {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    
+    // await client.close();
   }
 }
 run().catch(console.dir);
