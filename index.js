f6241a959969f293ea823ce989d3e64071422cb4
subornakter <subornakter01@gmail.com>
2025-11-12 22:49:00 +0600

    some change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require("cors");
+require("dotenv").config();
 const app = express()
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -7,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://ImportExport-db:bNqDEmyZ84wjVEvx@cluster0.r9l6yhe.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.r9l6yhe.mongodb.net/?appName=Cluster0`;
 app.get('/', (req, res) => {
   res.send('Server is running fine!')
 })
